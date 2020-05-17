75557d3d625437ba5038c72da85d63c05ecd9e55
Yutaka Okabe <yutaka.sh.6@gmail.com>
2020-05-18 00:12:58 +0900

    送信完了画面のひな形を作成

---
+++ join/thanks.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>会員登録</title>
+</head>
+<body>
+<h1>会員登録</h1>
+    <p>会員登録が完了しました</p>
+</body>
+</html>
