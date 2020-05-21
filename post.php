6f0c8aa5eac1a589ab73c0de6c978507b7706688
Yutaka Okabe <yutaka.sh.6@gmail.com>
2020-05-22 00:04:56 +0900

    投稿処理を追加

--- post.php
+++ post.php
@@ -5,7 +5,7 @@ require('dbconnect.php');
 if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()){
 // ログインしていて1時間以内にアクションがある場合
     $_SESSION['time'] = time();
-
+    // ユーザー情報を引き出す
     $members = $db->prepare('SELECT * FROM members WHERE id=?');
     $members->execute(array($_SESSION['id']));
     // ここではまだテーブルでいっぱい呼び出されていることになるので，fetch()で1行だけ取り出す
@@ -13,6 +13,20 @@ if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()){
 }else{
     header('Location: login.php');
 }
+
+if (!empty($_POST)){
+    if ($_POST['impression'] !== ''){
+        $impression = $db->prepare('INSERT INTO posts SET members_id=?, impression=?, created=NOW()');
+        $impression->execute(array(
+            $member['id'],
+            $_POST['impression']
+        ));
+        // $_POSTの値を消すために再読み込み
+        header('Location: index.php');
+        exit();
+    }
+}
+
 ?>
 
 <!DOCTYPE html> <!--HTML5に準拠したサイトならこれだけでOK　昔のにも対応したいならさらに記述が必要-->
@@ -26,20 +40,22 @@ if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()){
 <body>
     <h1>うまかもんポスト</h1>
     <p><?php print(htmlspecialchars($member['name'], ENT_QUOTES)); ?>さんのおいしいお店の情報をシェアしよう</p>
-    <dl>
-        <dt>お店の感想を入力</dt><!--definition term-->
-        <dd><!--definition description-->
-            <textarea name="impression" cols="100" rows="5" placeholder="ゴマサバが絶品だった！"></textarea>
-            <!--col:文字数rows:行数,textareaタグ内は入力初期値-->
-            <!--placeholder:半透明の入力例，required:入力必須指定-->
-            <input type="hidden" name="" value=""><!--hiddenはサイト上には表示されないが，サーバーに値を送信できる-->
-        </dd>
-        <dt>お店のGoogleMapのURLを入力</dt>
-        <dd>
-            <input type="text" name="url" size="122">
-        </dd>
-    </dl>
-    <input type="submit" value="投稿する">
-
+    <form action="" method="post">
+        <dl>
+            <dt>お店の感想を入力</dt><!--definition term-->
+            <dd><!--definition description-->
+                <textarea name="impression" cols="100" rows="5" placeholder="ゴマサバが絶品だった！"></textarea>
+                <!--col:文字数rows:行数,textareaタグ内は入力初期値-->
+                <!--placeholder:半透明の入力例，required:入力必須指定-->
+                <input type="hidden" name="" value="">
+                <!--hiddenはサイト上には表示されないが，サーバーに値を送信できる-->
+            </dd>
+            <dt>お店のGoogleMapのURLを入力</dt>
+            <dd>
+                <input type="text" name="url" size="122">
+            </dd>
+        </dl>
+        <input type="submit" value="投稿する">
+    </form>
 </body>
 </html>
