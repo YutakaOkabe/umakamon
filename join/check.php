69e03b7d9e377ba10592a4aa75fd0dda9a56dfe4
Yutaka Okabe <yutaka.sh.6@gmail.com>
2020-05-31 00:50:11 +0900

    Bootstrapで表示をきれいに

--- join/check.php
+++ join/check.php
@@ -27,44 +27,75 @@ if(!empty($_POST)){
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="css/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>会員登録</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <!--
+    <link rel="stylesheet" href="css/style.css">
+    -->
+
+    <!-- 画像を表示するCSS -->
+    <style>
+    .imagePreview {
+        width: 180px;
+        height: 180px;
+        background-position: center center;
+        background-size: cover;
+        -webkit-box-shadow: 0 0 1px 1px rgba(0, 0, 0, .3);
+        display: inline-block;
+    }
+    </style>
+
+    <title>アカウント作成</title>
 </head>
 <body>
-<h1>会員登録</h1>
-<p>登録内容を確認してください</p>
-<form action="" method="post">
-    <input type="hidden" name="action" value="submit">
-    <!-- 変数actionに値submitという値を入れた隠し要素を入力しているので，「登録する」を押すとこの隠し要素が送信される-->
-    <dl>
-        <dt>ニックネーム</dt>
-        <dd>
-            <?php print(htmlspecialchars($_SESSION['join']['name'], ENT_QUOTES)) ?>
-        </dd>
-        <dt>メールアドレス</dt>
-        <dd>
-            <?php print(htmlspecialchars($_SESSION['join']['email'], ENT_QUOTES)) ?>
-        </dd>
-        <dt>パスワード</dt>
-        <dd>
-            <?php 
-                $password_len = strlen($_SESSION['join']['password']);
-                for ($i=1; $i<=$password_len; $i++){
-                    print('*');
-                }
-            ?>
-        </dd>
-        <dt>写真など</dt>
-        <dd>
-            <?php if(!empty($_SESSION['join']['image'])): ?>
-            <img src="../member_picture/<?php print(htmlspecialchars($_SESSION['join']['image'], ENT_QUOTES)); ?>" alt="ユーザの写真" width="200" height="200">
-            <?php endif; ?>
-        </dd>
-    </dl>
-    <a href="index.php?action=rewrite">&laquo;&nbsp;書き直す</a> | <input type="submit" value="登録する">
-    <!-- &laquo; '<' , &nbsp; 半角スペース-->
-</form>
-    
+<img src="../image/logo.png" class="rounded mx-auto mt-5 d-block" alt="うまかもんロゴ">
+<div class="container">
+    <div class="text-center">
+        <p class="mt-3">登録内容を確認してください</p>
+        <form action="" method="post">
+            <input type="hidden" name="action" value="submit">
+            <!-- 変数actionに値submitという値を入れた隠し要素を入力しているので，「登録する」を押すとこの隠し要素が送信される-->
+                <table class="table table-striped">
+                    <tbody>
+                        <tr>
+                            <th scope="row">ニックネーム</th>
+                            <td><?php print(htmlspecialchars($_SESSION['join']['name'], ENT_QUOTES)) ?></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">メールアドレス</th>
+                            <td><?php print(htmlspecialchars($_SESSION['join']['email'], ENT_QUOTES)) ?></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">パスワード</th>
+                            <td><?php 
+                        $password_len = strlen($_SESSION['join']['password']);
+                        for ($i=1; $i<=$password_len; $i++){
+                            print('*');
+                        }
+                    ?>
+                        </tr>
+                        <tr>
+                            <th scope="row">アイコン</th>
+                            <!-- 画像指定してない場合でも日時が保存されているので文字数で判別 -->
+                            <?php if(strlen(($_SESSION['join']['image'])) === 14): ?>
+                            <td>指定なし</td>
+                            <?php else: ?>               
+                            <td>
+                                <img src="../member_picture/<?php print(htmlspecialchars($_SESSION['join']['image'], ENT_QUOTES)); ?>" class="imagePreview">
+                            </td>
+                            <?php endif; ?>
+            
+                        </tr>
+                    </tbody>
+                </table>
+            <div class="row justify-content-around mt-5">
+                <a class="btn btn-secondary col-3" href="index.php?action=rewrite" role="button">書き直す</a>
+                <button type="submit" class="btn btn-primary col-3">登録する</button>
+            </div>
+        </form>
+    </div>
+</div>
 </body>
 </html>
--- join/thanks.php
+++ join/thanks.php
@@ -2,12 +2,23 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="css/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>会員登録</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <!--
+    <link rel="stylesheet" href="css/style.css">
+    -->
+
+    <title>アカウント作成</title>
 </head>
 <body>
-<h1>会員登録</h1>
-    <p>会員登録が完了しました</p>
+<img src="../image/logo.png" class="rounded mx-auto mt-5 d-block" alt="うまかもんロゴ">
+<div class="container">
+    <div class="text-center">
+        <p class="mt-4">会員登録が完了しました</p>
+        <a href="../login.php">ログイン画面へ</a>
+    </div>
+</div>
 </body>
 </html>
